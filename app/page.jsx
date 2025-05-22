2248c6713380f1b5df105e82b0ce053c1699baa0
varscript <fvargas.edu@gmail.com>
2025-05-21 19:56:10 -0500

    Refactor server to client component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,32 @@
+"use client";
 import Link from "next/link";
 import Courses from "./componets/Courses";
+import { useEffect, useState } from "react";
+import LoadingPage from "./loading";
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCoures = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCoures();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
       <h1>Courses of your interest</h1>
-      <Courses />
+      <Courses courses={courses} />
     </>
   );
 };
